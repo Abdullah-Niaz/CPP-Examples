8c1aeeabed38928fceb0015c58def75d06ab8d4c
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-24 09:27:41 +0500

    Take input form user and print out

--- 2.Print_Number_Entered_by_User.cpp
+++ 2.Print_Number_Entered_by_User.cpp
@@ -7,7 +7,7 @@ int main()
     int x, y;
     cout << "Enter the Values: ";
     cin >> x >> y;
-    cout << "First Value is " << x << endl
-         << "Second Value is " << y;
+    cout << "First Value is: " << x << endl
+         << "Second Value is: " << y;
     return 0;
 }
