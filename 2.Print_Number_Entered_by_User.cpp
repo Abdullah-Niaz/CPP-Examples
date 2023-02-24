3448f3f1b93af39292e0927dfdacad25ffc9ffca
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-24 09:23:41 +0500

    Basics Examples

---
+++ 2.Print_Number_Entered_by_User.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Take input from user and store it to variable
+    int x, y;
+    cout << "Enter the Values: ";
+    cin >> x >> y;
+    cout << "First Value is " << x << endl
+         << "Second Value is " << y;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CPP-Examples
+
 The best way to learn CPP is by practicing examples. This repo contains examples on basic concepts of CPP. I encourage you to try these examples on your own before looking at the solution.
