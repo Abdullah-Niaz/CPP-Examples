f03c9315c835e48a5efafbd463fa442613f51ac2
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-24 10:15:13 +0500

    Product

---
+++ 8.Multiply_Two_Numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, product;
+    cout << "Enter the a,b Values a & b repectively: ";
+    cin >> a >> b;
+    product = a * b;
+    cout << "Product: " << product << endl;
+    return 0;
+}
