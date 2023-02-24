59513e0a70bebdaaf6792a95c2e3ef31c7c2c3c3
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-24 09:29:29 +0500

    Size of Data Types

--- 5.Find_Size_of_int_float_double_and_char_in_Your_System.cpp
+++ 5.Find_Size_of_int_float_double_and_char_in_Your_System.cpp
@@ -11,11 +11,11 @@ int main()
     double e;
     long f;
 
-    cout << "Size of int" << sizeof(a) << endl;
-    cout << "Size of float" << sizeof(b) << endl;
-    cout << "Size of char" << sizeof(c) << endl;
-    cout << "Size of string" << sizeof(d) << endl;
-    cout << "Size of double" << sizeof(e) << endl;
-    cout << "Size of long" << sizeof(f) << endl;
+    cout << "Size of int:" << sizeof(a) << endl;
+    cout << "Size of float:" << sizeof(b) << endl;
+    cout << "Size of char:" << sizeof(c) << endl;
+    cout << "Size of string:" << sizeof(d) << endl;
+    cout << "Size of double:" << sizeof(e) << endl;
+    cout << "Size of long:" << sizeof(f) << endl;
     return 0;
 }
